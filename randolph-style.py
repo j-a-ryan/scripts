a7afe0e461012c7a82015fd58077b490ceef9299
Jim Ryan <j.a.ryan@protonmail.com>
2026-08-24 09:28:13 -0400

    With local venv installation, updates to Pandas 3.xx, Python 3.14 and makes various little fixes flagged by Pylance.

--- randolph-style.py
+++ randolph-style.py
@@ -1,68 +1,79 @@
 import os
+
 import pandas as pd
+
 import lookup
 import utilities as util
 
+"""
+LGT 0
+LGF 2973
+P1 1810
+O 1793
+TB 1991
+R 176
+"""
+
 ########## Comment/uncomment to suit data ###############
-R = 'randolph'# Two pairs of temp/leaf wetness
-S = 'singleton' # One pair
-SLG_T = 'singleton-low-gap-tables' # One pair, but with Low Gap naming
-data_style = R 
+R = "randolph"  # Two pairs of temp/leaf wetness
+S = "singleton"  # One pair
+SLG_T = "singleton-low-gap-tables"  # One pair, but with Low Gap naming
+data_style = R
 # data_style = S
 # data_style = SLG_T
 #########################################################
 
 ## Some global constants
-cleaned_file_name = 'cleaned.csv'
-truncated_file_name = 'truncated.csv'
-smoothed_file_name = 'smoothed.csv'
-data_dir = '../'
+cleaned_file_name = "cleaned.csv"
+truncated_file_name = "truncated.csv"
+smoothed_file_name = "smoothed.csv"
+data_dir = "../"
 
-latest_raw_data_dir = data_dir + 'latest/'
+latest_raw_data_dir = data_dir + "latest/"
 cleaned_file_path = data_dir + cleaned_file_name
 truncated_file_path = data_dir + truncated_file_name
 smoothed_file_path = data_dir + smoothed_file_name
 
-columns_wanted = ''
+columns_wanted = ""
 if data_style == R:
     columns_wanted = lookup.columns_wanted_randolph_type_station
 elif data_style == S:
     columns_wanted = lookup.columns_wanted_singleton_station
 elif data_style == SLG_T:
     columns_wanted = lookup.columns_wanted_singleton_water_tables_low_gap_station
-temp_1_field_name = ''
-temp_2_field_name = ''
-leaf_wetness_1_field_name = ''
-leaf_wetness_2_field_name = ''
+temp_1_field_name = ""
+temp_2_field_name = ""
+leaf_wetness_1_field_name = ""
+leaf_wetness_2_field_name = ""
 datetime_field_name = lookup.date_time_field_name
-station_name = ''
-transmitter_names = [] # Davis calls some of these 'stations'
+station_name = ""
+transmitter_names = []  # Davis calls some of these 'stations'
 temp_field_names = []
 leaf_wetness_field_names = []
 if len(columns_wanted) <= 2:
     for col in columns_wanted:
-        if 'Date' in col:
+        if "Date" in col:
             datetime_field_name = col
-        elif 'Temp' in col:
+        elif "Temp" in col:
             temp_field_name = col
             temp_field_names.append(temp_field_name)
-        elif 'Leaf' in col:
+        elif "Leaf" in col:
             leaf_wetness_field_name = col
             leaf_wetness_field_names.append(leaf_wetness_field_name)
 else:
     for col in columns_wanted:
-        if 'Date' in col:
+        if "Date" in col:
             datetime_field_name = col
-        elif 'Temp' in col and '1' in col:
+        elif "Temp" in col and "1" in col:
             temp_1_field_name = col
             temp_field_names.append(col)
-        elif 'Temp' in col and '2' in col:
+        elif "Temp" in col and "2" in col:
             temp_2_field_name = col
             temp_field_names.append(col)
-        elif 'Leaf' in col and '1' in col:
+        elif "Leaf" in col and "1" in col:
             leaf_wetness_1_field_name = col
             leaf_wetness_field_names.append(col)
-        elif 'Leaf' in col and '2' in col:
+        elif "Leaf" in col and "2" in col:
             leaf_wetness_2_field_name = col
             leaf_wetness_field_names.append(col)
 
@@ -76,23 +87,26 @@ file_name = files[0]
 print("Found the file: " + file_name)
 full_file_path = latest_raw_data_dir + file_name
 print("Opening file to find station name and transmitter names....")
-with open(full_file_path, mode='r') as raw_file:
+with open(full_file_path, mode="r") as raw_file:
     print(raw_file.readline())
-    station_name = raw_file.readline().strip('\n').strip('"').strip()  # Read station name from file.
-    raw_transmitter_names_str = raw_file.readlines()[1].strip('\n').strip().replace('"', '') # A line has been read; the new line 1 should have the transmitter names.
-    if raw_transmitter_names_str.startswith(','):
-        raw_transmitter_names_str = raw_transmitter_names_str[1:]    
-    transmitter_names = list(set(raw_transmitter_names_str.split(',')))
-    
+    station_name = raw_file.readline().strip("\n").strip('"').strip()  # Read station name from file.
+    raw_transmitter_names_str = (
+        raw_file.readlines()[1].strip("\n").strip().replace('"', "")
+    )  # A line has been read; the new line 1 should have the transmitter names.
+
+    raw_transmitter_names_str = raw_transmitter_names_str.removeprefix(",")
+
+    transmitter_names = list(set(raw_transmitter_names_str.split(",")))
+
 print("File contents show:")
-print('\tstation name: ' + station_name)
+print("\tstation name: " + station_name)
 print("\ttransmitter name(s): ")
 for name in transmitter_names:
-    print('\t\t' + name)          
+    print("\t\t" + name)
 ########################## 2. Clean the data, creating cleaned.csv #############################
 print("Opening file again for data cleaning, creating cleaned file called " + cleaned_file_name + "....")
 util.clean(raw_file_path=full_file_path, cleaned_file_path=cleaned_file_path)
-        
+
 ########################## 3. Eliminate unwanted columns #######################################
 # Next get a convenient truncated file with only the data we need, using the cleaned file.
 util.truncate(cleaned_file_path=cleaned_file_path, truncated_file_path=truncated_file_path)
@@ -100,9 +114,11 @@ util.truncate(cleaned_file_path=cleaned_file_path, truncated_file_path=truncated
 ########################## 4. Smooth the data to hourly ########################################
 # Loop through the rows, smoothing the data to hourly from every 15 min : take mean of each four rows for each field
 truncated_file_df = pd.read_csv(truncated_file_path)  # Get the truncated data from the truncated file.
-smoothed_df = util.smooth(truncated_file_df=truncated_file_df, # Create a dataframe that contains the smoothed data.
-                          temp_field_names=temp_field_names, 
-                          leaf_wetness_field_names=leaf_wetness_field_names)
+smoothed_df = util.smooth(
+    truncated_file_df=truncated_file_df,  # Create a dataframe that contains the smoothed data.
+    temp_field_names=temp_field_names,
+    leaf_wetness_field_names=leaf_wetness_field_names,
+)
 
 
 # Write the smoothed file
@@ -119,8 +135,10 @@ nonzero_risk_periods = []  # All the risk periods for this file, including all l
 
 # Loop through the analysis, using lookup to add risk units (if > 0) to risk_units_1_vals and risk_units_2_vals
 # If a row has zero units to add to a tally and the tally > 0, then add to dictionary, key: start time, val: [ru, end time]
-for i in range(len(temp_field_names)): # assumes lw and temp name arrays same length
-    nonzero_risk_period_for_this_lw_temp_pair = util.analyze(analysis_df=analysis_df, leaf_wetness_field_name=leaf_wetness_field_names[i], temp_field_name=temp_field_names[i])
+for i in range(len(temp_field_names)):  # assumes lw and temp name arrays same length
+    nonzero_risk_period_for_this_lw_temp_pair = util.analyze(
+        analysis_df=analysis_df, leaf_wetness_field_name=leaf_wetness_field_names[i], temp_field_name=temp_field_names[i]
+    )
     nonzero_risk_periods.append(nonzero_risk_period_for_this_lw_temp_pair)
 
 ########################## 6. Report results of analysis #######################################
@@ -136,17 +154,14 @@ for i in range(len(nonzero_risk_periods)):
         print("...")
     else:
         print(f"There were no risk periods for sensor pair {i}.\n")
-        
+
 highest_rank_wet = 0
 highest_rank_with_dry_time = 0
 highest_rank_with_half_dry_time = 0
 for ranking in rankings:
-    if ranking[0] > highest_rank_wet:
-        highest_rank_wet = ranking[0]
-    if ranking[1] > highest_rank_with_dry_time:
-        highest_rank_with_dry_time = ranking[1]
-    if ranking[1] > highest_rank_with_half_dry_time:
-        highest_rank_with_half_dry_time = ranking[1]
+    highest_rank_wet = max(ranking[0], highest_rank_wet)
+    highest_rank_with_dry_time = max(ranking[1], highest_rank_with_dry_time)
+    highest_rank_with_half_dry_time = max(ranking[1], highest_rank_with_half_dry_time)
 print("Risk level for wet time: " + util.get_risk_level(highest_rank_wet)[0])
 print("Risk level incl full dry time: " + util.get_risk_level(highest_rank_with_dry_time)[0])
 print("Risk level incl half dry time: " + util.get_risk_level(highest_rank_with_half_dry_time)[0])
@@ -164,7 +179,7 @@ for i in range(len(nonzero_risk_periods)):
     infection_period_ru_avg = 0
     nonzero_risk_period = nonzero_risk_periods[i]
     if len(nonzero_risk_period) > 0:
-        for key, value in nonzero_risk_period.items():
+        for value in nonzero_risk_period.values():
             total_ru = value[0]
             dry_ru = value[1]
             end_time = value[2]
@@ -180,13 +195,9 @@ for i in range(len(nonzero_risk_periods)):
         print("Total (incl dry-off) inf period RU: " + str(infection_period_ru))
         print("Wet-only (no dry-off) inf period RU: " + str(infection_period_ru_wet))
         print("Avg (allowing half dry-off time) inf period RU: " + str(infection_period_ru_avg))
-        if infection_period_ru > net_all_sensor_pairs_infection_period_ru:
-            net_all_sensor_pairs_infection_period_ru = infection_period_ru
-        if infection_period_ru_wet > net_all_sensor_pairs_infection_period_ru_wet:
-            net_all_sensor_pairs_infection_period_ru_wet = infection_period_ru_wet
-        if infection_period_ru_avg > net_all_sensor_pairs_infection_period_ru_avg:
-            net_all_sensor_pairs_infection_period_ru_avg = infection_period_ru_avg
-        
+        net_all_sensor_pairs_infection_period_ru = max(infection_period_ru, net_all_sensor_pairs_infection_period_ru)
+        net_all_sensor_pairs_infection_period_ru_wet = max(infection_period_ru_wet, net_all_sensor_pairs_infection_period_ru_wet)
+        net_all_sensor_pairs_infection_period_ru_avg = max(infection_period_ru_avg, net_all_sensor_pairs_infection_period_ru_avg)
 
 print("\n\nNet for all sensor pairs total RUs:")
 print("Total (incl dry-off) inf period RU: " + str(net_all_sensor_pairs_infection_period_ru))
--- utilities.py
+++ utilities.py
@@ -1,12 +1,15 @@
-import lookup
-import pandas as pd
 import numbers
+from typing import cast
+
+import pandas as pd
+
+import lookup
 
 datetime_field_name = lookup.date_time_field_name
 num_dry_hours_to_stop_cycle = lookup.hours_drying_time  # If this is set to zero, code will probably need updating
 
 
-# Analyze the data for a given temp field name and leaf wetness field name. Find discrete risk periods (period of nonzero risk) 
+# Analyze the data for a given temp field name and leaf wetness field name. Find discrete risk periods (period of nonzero risk)
 # and calculate and assign risk unit totals for each period. Place results in dictionary, which has structure:
 #           start time mapping to array of [ru total, final dry ru total, end time]
 def analyze(analysis_df: pd.DataFrame, leaf_wetness_field_name, temp_field_name) -> dict:
@@ -14,7 +17,7 @@ def analyze(analysis_df: pd.DataFrame, leaf_wetness_field_name, temp_field_name)
 
     # Loop through the analysis, using lookup to add risk units (if > 0) to risk_units_1_vals and risk_units_2_vals
     # If a row has zero units to add to a tally and the tally > 0, then add to dictionary, key: start time, val: [ru, end time]
-    start_time = ''
+    start_time = ""
     num_rows = len(analysis_df)
     num_dry_hours = 0
     num_final_dry_risk_units = 0
@@ -28,28 +31,40 @@ def analyze(analysis_df: pd.DataFrame, leaf_wetness_field_name, temp_field_name)
             num_dry_hours = 0  # ... then zero out these values.
             num_final_dry_risk_units = 0
         elif risk_units_running_total > 0:  # ...else if it's dry but we're still in an infection cycle....:
-            num_dry_hours += 1 # Each row is one hour
-        
-        # If either leaf wetness is above zero or we're still in a dry part of an infection cycle, then update the corresponding risk unit total. 
-        # Otherwise, if there have been sufficient hours of dry time to stop the infection, then the risk event is finished. Update the corresponding 
+            num_dry_hours += 1  # Each row is one hour
+
+        # If either leaf wetness is above zero or we're still in a dry part of an infection cycle, then update the corresponding risk unit total.
+        # Otherwise, if there have been sufficient hours of dry time to stop the infection, then the risk event is finished. Update the corresponding
         # dictionary with its data and zero out its risk unit var. Start with sensor pair 1
         if lw > 0 or (risk_units_running_total > 0 and num_dry_hours < num_dry_hours_to_stop_cycle):
-            if temp in lookup.risk_units_lookup:  # If it's not very cold or very hot (We are not considering heat as killing the fungus or stopping the infection)
-                if (not start_time):  # If we need to grab the start date for a new non-zero row for our results
+            if (
+                temp in lookup.risk_units_lookup
+            ):  # If it's not very cold or very hot (We are not considering heat as killing the fungus or stopping the infection)
+                if not start_time:  # If we need to grab the start date for a new non-zero row for our results
                     start_time = datetime
                 ru = lookup.risk_units_lookup[temp]
                 risk_units_running_total += ru
                 if num_dry_hours > 0:  # If we're possibly in final drying....
                     num_final_dry_risk_units += ru  # Add to the final dry risk units total.
-                if i == num_rows - 1:  # If at end of file, finish the risk period. It is continuing, but there is nothing we can do about that. Make a new, extended file that has the enire risk period in it.
-                    nonzero_risk_periods_for_this_sensor_pair[start_time] = [round(risk_units_running_total), round(num_final_dry_risk_units), datetime]  # start time maps to array of ru, final dry ru, end time
+                if (
+                    i == num_rows - 1
+                ):  # If at end of file, finish the risk period. It is continuing, but there is nothing we can do about that. Make a new, extended file that has the enire risk period in it.
+                    nonzero_risk_periods_for_this_sensor_pair[start_time] = [
+                        round(risk_units_running_total),
+                        round(num_final_dry_risk_units),
+                        datetime,
+                    ]  # start time maps to array of ru, final dry ru, end time
         else:
             if num_dry_hours >= num_dry_hours_to_stop_cycle or i == num_rows - 1:  # If we have finished drying or finished the file.
                 if risk_units_running_total > 0:
-                    nonzero_risk_periods_for_this_sensor_pair[start_time] = [round(risk_units_running_total), round(num_final_dry_risk_units), datetime]  # start time maps to array of ru, final dry ru, end time
-                
+                    nonzero_risk_periods_for_this_sensor_pair[start_time] = [
+                        round(risk_units_running_total),
+                        round(num_final_dry_risk_units),
+                        datetime,
+                    ]  # start time maps to array of ru, final dry ru, end time
+
                 # Reset the variables
-                start_time = ''
+                start_time = ""
                 risk_units_running_total = 0
                 num_dry_hours = 0
     return nonzero_risk_periods_for_this_sensor_pair
@@ -58,7 +73,7 @@ def analyze(analysis_df: pd.DataFrame, leaf_wetness_field_name, temp_field_name)
 def smooth(truncated_file_df: pd.DataFrame, temp_field_names, leaf_wetness_field_names):
     # Loop through the rows. Grab date from first row of every group of four rows, keeping arrays for calculating means.
     new_row_dict = {}
-    smoothed_df = pd.DataFrame
+    smoothed_df = pd.DataFrame()
     # These arrays will hold the arrays(s) of four values for calculating means
     temps = []
     lws = []
@@ -66,15 +81,18 @@ def smooth(truncated_file_df: pd.DataFrame, temp_field_names, leaf_wetness_field
         temps.append([])
     for name in leaf_wetness_field_names:
         lws.append([])
-    
+
     for index, row in truncated_file_df.iterrows():
-        
+        index = cast(int, index)
+
         # Check whether beginning a group of four records:
-        if index % 4 == 0 or index == len(truncated_file_df) + 1:  # If at beginning/end of group of 4, including end of file (may be group of fewer than 4 which we'll count as a group)
-            if index > 0: # If it's not the first row
-                # Calculate means, Use them in new df row. The lengths of the lists should be four 
+        if (
+            index % 4 == 0 or index == len(truncated_file_df) + 1
+        ):  # If at beginning/end of group of 4, including end of file (may be group of fewer than 4 which we'll count as a group)
+            if index > 0:  # If it's not the first row
+                # Calculate means, Use them in new df row. The lengths of the lists should be four
                 # but may be smaller if there was dirty nonumeric data in this group of four. When
-                # this script cleans all dirty data (see interpolate issue above) then the groups will always be 
+                # this script cleans all dirty data (see interpolate issue above) then the groups will always be
                 # groups of four (except perhaps the last group in the file.)
                 for i, name in enumerate(temp_field_names):
                     if len(temps[i]) == 0:
@@ -90,26 +108,27 @@ def smooth(truncated_file_df: pd.DataFrame, temp_field_names, leaf_wetness_field
                     else:
                         mean_lw = round(sum(lws[i]) / len(lws[i]))
                     new_row_dict[name] = mean_lw
-                
+
+                new_row_df = pd.DataFrame(new_row_dict, index=[0])
                 if smoothed_df.empty:
-                    smoothed_df = pd.DataFrame(new_row_dict, index=[0])
+                    smoothed_df = new_row_df
                 else:
-                    smoothed_df = smoothed_df._append(new_row_dict, ignore_index=True)
-                
+                    smoothed_df = pd.concat([smoothed_df, new_row_df], ignore_index=True)
+
                 # Clear the lists
                 for i in range(len(temps)):
                     temps[i].clear()
                 for i in range(len(lws)):
                     lws[i].clear()
                 new_row_dict = {}  # Just because why not.
-            
+
             # Start a new row. We are on the next row of data now.
             new_row_dict[datetime_field_name] = row[datetime_field_name]
-        
+
         if index <= len(truncated_file_df):
             # We are working on a group of four. Increment the vars, checking for dirty values, such as "--"
             # N.B.: we are assuming that all numbers are ints.
-            
+
             for i, name in enumerate(temp_field_names):
                 if isinstance(row[name], numbers.Number) or (isinstance(row[name], str) and row[name].isnumeric()):
                     temps[i].append(int(row[name]))
@@ -117,27 +136,28 @@ def smooth(truncated_file_df: pd.DataFrame, temp_field_names, leaf_wetness_field
                 if isinstance(row[name], numbers.Number) or (isinstance(row[name], str) and row[name].isnumeric()):
                     lws[i].append(int(row[name]))
     return smoothed_df
-            
+
 
 def truncate(cleaned_file_path, truncated_file_path):
     print("Now creating truncated file....")
     cleaned_file_df = pd.read_csv(cleaned_file_path)  # assumes encoding='utf-8' from cleaned file, as above
     columns_to_remove = lookup.columns_unwanted_randolph_type_station
     for col in columns_to_remove:
-        if col in cleaned_file_df.columns: # Some abbreviated raw files do not have the unwanted columns to begin with.
+        if col in cleaned_file_df.columns:  # Some abbreviated raw files do not have the unwanted columns to begin with.
             cleaned_file_df.pop(col)
-    cleaned_file_df.to_csv(truncated_file_path) #, index=False)  # Pandas creates an index column by default. We'll use it
-    print('Created truncated file: truncated.csv')
-    
+    cleaned_file_df.to_csv(truncated_file_path)  # , index=False)  # Pandas creates an index column by default. We'll use it
+    print("Created truncated file: truncated.csv")
+
+
 def clean(raw_file_path, cleaned_file_path):
     dirty_lines_count = 0
-    with open(raw_file_path, mode='r') as raw_file, open(cleaned_file_path, mode='w', encoding='utf-8') as cleaned_file:
+    with open(raw_file_path, mode="r") as raw_file, open(cleaned_file_path, mode="w", encoding="utf-8") as cleaned_file:
         lines_without_header = raw_file.readlines()[5:]  # Skip the header. [5:] should start on the column names row.
         for line in lines_without_header:
-            cleaned_line = line.replace('"', '').replace("'", '')
-            if '--' in cleaned_line: # TODO: any other non-numeric strings possible?
-                print("Found line with \"--\" in it")
-                dirty_lines_count +=1
+            cleaned_line = line.replace('"', "").replace("'", "")
+            if "--" in cleaned_line:  # TODO: any other non-numeric strings possible?
+                print('Found line with "--" in it')
+                dirty_lines_count += 1
             cleaned_file.write(cleaned_line)
 
     print("Data has been cleaned, cleaned.csv created. Checking for additional issues (NaN, etc.) requiring additional cleaning....")
@@ -156,7 +176,7 @@ def clean(raw_file_path, cleaned_file_path):
         # # cleaned_file_df.interpolate(method='linear', inplace=True)
         # cleaned_file_df.to_csv(cleaned_file2_path)
 
-        
+
 def get_risk_level(ru: int):
     risk_pair = ["No Risk", 0]
     if ru > 400:
@@ -175,6 +195,7 @@ def get_risk_level(ru: int):
         risk_pair[0] = "Slight Risk"
         risk_pair[1] = 2
     return risk_pair
+
 
 def printout(risk_dict, rankings):
     for key, value in risk_dict.items():
